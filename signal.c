d01a3e1982e1c5b6127827e6428924a94cea48e0
lhu10 <60020940+lhu10@users.noreply.github.com>
2020-12-01 12:46:14 -0500

    Update signal.c

--- signal.c
+++ signal.c
@@ -2,10 +2,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <fcntl.h>
+#include <errno.h>
 
 static void int_handler(int signo){
     if(signo == SIGINT){
-        printf("Program Exited due to SIGINT\n");
+        int fd, result; 
+        char msg[] = "Program Exited due to SIGINT\n");
+        fd = open("output.txt", O_APPEND | O_WRONLY | O_CREAT, 0644);
+        if(fd == -1){
+            printf("Error %d: %s\n", errno, strerror(errno));
+            return -1;
+        }
+        result = write(fd, msg, sizeof(msg)-1);
+        if(result == -1){
+            printf("Error %d: %s\n", errno, strerror(errno));
+            return -1; 
+        }
+        close(fd);
         exit(0);
     }
 }
